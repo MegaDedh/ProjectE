d6add19395ba14c21d01c71f7168cc9e58857e27
MegaDedh <ToyotaCami813!@#>
2019-01-31 16:56:36 +0700

    Commit Work

--- p1061_fragmentactivity/src/main/java/ru/startandroid/p1061_fragmentactivity/MainActivity.java
+++ p1061_fragmentactivity/src/main/java/ru/startandroid/p1061_fragmentactivity/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,16 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction ft = getFragmentManager().beginTransaction();
         ft.add(R.id.fragment2, frag2);
         ft.commit();
+
     }
+    public void onClick(View v) {
+        Fragment frag1 = getFragmentManager().findFragmentById(R.id.fragment1);
+        ((TextView) frag1.getView().findViewById(R.id.textView))
+                .setText("Access to Fragment 1 from Activity");
+
+        Fragment frag2 = getFragmentManager().findFragmentById(R.id.fragment2);
+        ((TextView) frag2.getView().findViewById(R.id.textView))
+                .setText("Access to Fragment 2 from Activity");
+    }
+
 }
