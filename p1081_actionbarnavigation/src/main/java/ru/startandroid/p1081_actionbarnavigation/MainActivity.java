1782af7d1ccab3addecafd7c02108945d11613eb
MegaDedh <ToyotaCami813!@#>
2019-02-01 15:55:34 +0700

    Commit Work

---
+++ p1081_actionbarnavigation/src/main/java/ru/startandroid/p1081_actionbarnavigation/MainActivity.java
@@ -0,0 +1,55 @@
+package ru.startandroid.p1081_actionbarnavigation;
+
+
+
+
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.FragmentTransaction;
+import android.util.Log;
+
+
+
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        ActionBar bar = getActionBar();
+
+        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.Tab tab = bar.newTab();
+        tab.setText("tab1");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+        tab = bar.newTab();
+        tab.setText("tab2");
+        tab.setTabListener(this);
+        bar.addTab(tab);
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "reselected tab: " + tab.getText());
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "selected tab: " + tab.getText());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        Log.d(LOG_TAG, "unselected tab: " + tab.getText());
+    }
+}
