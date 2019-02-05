8a11d90bafe3ef0fdb7d8f0b742acf3594fd21d1
MegaDedh <ToyotaCami813!@#>
2019-02-05 16:13:18 +0700

    Commit Work

--- p1091_listfragment/src/main/java/ru/startandroid/p1091_listfragment/MainList.java
+++ p1091_listfragment/src/main/java/ru/startandroid/p1091_listfragment/MainList.java
@@ -2,7 +2,11 @@ package ru.startandroid.p1091_listfragment;
 
 import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainList extends ListFragment {
     String data[] = new String[] { "one", "two", "three", "four" };
@@ -12,6 +16,16 @@ public class MainList extends ListFragment {
         super.onActivityCreated(savedInstanceState);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
                 android.R.layout.simple_list_item_1, data);
+//layout.simple_list_item_activated_1
+//layout.simple_list_item_1
+
+
         setListAdapter(adapter);
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment, null);
     }
 }
